6fff00ed83accd5aced266fe142d8319bf9f13b1
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-07 12:39:36 -0600

    Added a start method and a main method

--- src/garbage/controller/GarbageController.java
+++ src/garbage/controller/GarbageController.java
@@ -2,5 +2,9 @@ package garbage.controller;
 
 public class GarbageController
 {
-
+	public void start()
+	{
+		System.out.print("ayyyy lmao.");
+		System.out.print("This project is garbage.");
+	}
 }
--- src/garbage/controller/GarbageRunner.java
+++ src/garbage/controller/GarbageRunner.java
@@ -2,8 +2,9 @@ package garbage.controller;
 
 public class GarbageRunner
 {
-	public static void main(String []
+	public static void main (String [ ] args)
 	{
-		
+		GarbageController appController = new GarbageController();
+		appController.start();
 	}
 }
