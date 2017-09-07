3a2a77b85567fce80e1e16c103a6aa8405935e39
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-07 12:41:49 -0600

    Changed the text in the output.

--- src/garbage/controller/GarbageController.java
+++ src/garbage/controller/GarbageController.java
@@ -4,7 +4,7 @@ public class GarbageController
 {
 	public void start()
 	{
-		System.out.print("ayyyy lmao.");
+		System.out.print("Ayyyy lmao.");
 		System.out.print("This project is garbage.");
 	}
 }
